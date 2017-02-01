9e3a95351476c3af263cedb1b8a74cf7118c22e4
Nick Pellegrino <nickpellegrino@nicks-mbp.dhcp.nd.edu>
2017-02-01 16:40:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/nickpellegrino/lab2_npelleg1/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.nickpellegrino.lab2_npelleg1;
+
+/**
+ * Created by nickpellegrino on 2/1/17.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
